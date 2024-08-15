a3c82524832708e70ea4691f059e0e5b5e492c13
Maksym Duda <maksimduda387@gmail.com>
2024-08-15 19:08:14 +0300

    fix: fixed missed assembly reference

--- AnnounceBoard.Infrastructure/obj/Debug/net8.0/AnnounceBoard.Infrastructure.AssemblyInfo.cs
+++ AnnounceBoard.Infrastructure/obj/Debug/net8.0/AnnounceBoard.Infrastructure.AssemblyInfo.cs
@@ -10,10 +10,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("AnnounceBoard.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef00be3ff615fac45a514f69f8efb50b90df5656")]
 [assembly: System.Reflection.AssemblyProductAttribute("AnnounceBoard.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AnnounceBoard.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
